454ffeb7bfcb243c31363629b11bbbf97e6433de
Ronald Li <li.ronald2008@gmail.com>
2025-02-21 20:11:50 -0700

    add climb PID hold

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -77,6 +77,10 @@ public final class Constants {
     public static final int rightClimbMotorID = 42;
 
     public static final double maxClimbSpeed = 0.4;
+
+    public static final double kP = 0.1;
+    public static final double kI = 0;
+    public static final double kD = 0;
   }
 
   public static class ShooterConstants {
--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -4,12 +4,18 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.sim.SparkRelativeEncoderSim;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.generated.TunerConstants;
 
 public class ClimbSubsystem extends SubsystemBase {
     private double climbSpeed = 0;
@@ -19,28 +25,56 @@ public class ClimbSubsystem extends SubsystemBase {
     SparkFlexConfig leftConfig;
     SparkFlexConfig rightConfig;
 
+    private boolean hold;
+
+    private RelativeEncoder encoder = leftClimb.getEncoder();
+
+    private double curEncoderPosition;
+    private double curSetPosition;
+
+    private PIDController climbPID = new PIDController(
+        Constants.ClimbConstants.kP,
+        Constants.ClimbConstants.kI,
+        Constants.ClimbConstants.kD
+    );
+
     public ClimbSubsystem() {
         leftConfig = new SparkFlexConfig();
 
         leftConfig.inverted(true);
 
         leftClimb.configure(leftConfig, null, null);
+
+        hold = false;
+        curEncoderPosition = encoder.getPosition();
+        curSetPosition = curEncoderPosition;
     }
 
     @Override
     public void periodic(){
+        curEncoderPosition = encoder.getPosition();
+        if(hold) {
+            climbPID.setSetpoint(curSetPosition);
+            climbSpeed = MathUtil.clamp(climbPID.calculate(curEncoderPosition), 
+                                            -Constants.ClimbConstants.maxClimbSpeed, 
+                                            Constants.ClimbConstants.maxClimbSpeed);
+        }
         leftClimb.set(climbSpeed);
     }
 
     public void climbUp() {
         climbSpeed = Constants.ClimbConstants.maxClimbSpeed;
+        hold = false;
     }
 
     public void climbDown() {
         climbSpeed = -Constants.ClimbConstants.maxClimbSpeed;
+        hold = false;
     }
 
     public void stop() {
         climbSpeed = 0.0;
+        hold = true;
+        curSetPosition = curEncoderPosition;
     }
 }
