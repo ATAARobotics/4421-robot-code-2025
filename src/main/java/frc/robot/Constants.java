c068a6cb2393b6a32b655a7597ce0616de962a25
Ronald Li <li.ronald2008@gmail.com>
2025-02-23 13:08:52 -0700

    slow mode working?

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -107,6 +107,8 @@ public final class Constants {
 
     public static final int gyroCounter = 10;
     
+    public static final double linearDeadBand = 0.20;
+    public static final double angularDeadBand = 0.15;
   }
    
   
